13b3e7a2a56de9e00d9068786aa20f71e8670364
BishoyRafik <Bishoy.rafik77@gmail.com>
2023-07-14 15:27:38 +0300

    Updating Logic so it do not repeat a quote twice in a row

--- js/quote.js
+++ js/quote.js
@@ -1,3 +1,4 @@
+// Defining some quote objects and each object has 2 properties theQuote & author to be called later in paragraphs
 var quote1 = {
     theQuote : '“Be yourself; everyone else is already taken.”',
     author :'― Oscar Wilde',
@@ -18,39 +19,44 @@ var quote5 = {
     theQuote : '“Twenty years from now you will be more disappointed by the things that you did not do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.”',
     author :'― H. Jackson Brown Jr., P.S. I Love You',
 }
-var quotes = [quote1, quote2, quote3, quote4, quote5];
-var current;
+var quote6 = {
+    theQuote : '“Be who you are and say what you feel, because those who mind do not matter, and those who matter do not mind.”',
+    author :'― Bernard M. Baruch',
+}
+var quote7 = {
+    theQuote : '“Be the change that you wish to see in the world.”',
+    author :'― Mahatma Gandhi',
+}
+// Array of Objects to call objects randomly by index later
+var quotes = [quote1, quote2, quote3, quote4, quote5, quote6, quote7];
+var current; // variable to store current random number in order to compare with the generated random number later
 
+// main function called with action on click on button operates the program
 function generateQuote(){
-    // clearData();
-    console.log(quotes);
-    displayQuote();
-    console.log('random is ',random);
-    console.log('current is ',current);
-    
+    // console.log(quotes); //displays quotes array in console
+    displayQuote();    // calls displayQuote function
 }
 
+// function used to generate random number and get a quote according to this random number
 function displayQuote(){
-    var container = ''
-    var random  = Math.floor(Math.random()*(quotes.length + 1));
-    if(random == current){
-        generateQuote();
-        current = random;
-    } else {
+    var container = ''      //Empty container to be used in printing html code using JS 
+    var random  = Math.floor(Math.random()*quotes.length);      //variable generates random number from 0 to quotes array length which is dynamic if new quotes were added in the future
+    if(random == current){      //if the generated number is equal to the last generated one 
+        displayQuote();         //then it with recall itself recrusively to generate other random number
+    } else {        //when they are not equal it will add two paragraph tags to the container
+
+        //first P calls the quote object with index = random generated number to get the value of theQuote from it
+        //second P calls the quote object with same index = random generated number to get the value of author from it
         container +=
         `
-        <p class="quote my-3 bg-success-subtle rounded-2">${quotes[random].theQuote}</p>
+        <p class="quote my-3 bg-success-subtle rounded-2">${quotes[random].theQuote}</p> 
         <p class="author my-3 bg-success-subtle rounded-2">${quotes[random].author}</p>
-        `
-        document.getElementById("quotePlace").innerHTML=container;
-        current = random;
-    }
-}
+        `;
 
-// Doesn't need a clear function as it overwrites each time
-// function clearData(){
-//     document.getElementById('quotePlace').innerHTML = `
-//     <p class="quote my-3"></p>
-//     <p class="author my-3"></p>
-//     `
-// }
+        document.getElementById("quotePlace").innerHTML=container; //this line calls a tag with id quotePlace inside the html file to put the block of code stored in container inside it
+        current = random; //changing current to the new random generated
+    }
+
+    // console.log('current is ',current); //displays current in console
+    console.log('random is ',random); //displays random in console
+}
